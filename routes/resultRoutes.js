4a1fe79a18e48d294b4995a22ecb4b91311ce552
kendrick807 <tkogt12@gmail.com>
2023-11-06 22:31:36 +0200

    FPV-46 Use queries instead of aggregate blocks

--- routes/resultRoutes.js
+++ routes/resultRoutes.js
@@ -1,70 +1,51 @@
 const express = require('express');
 const resultRouter = express.Router();
 const User = require('../models/User');
+const Candidate = require('../models/Candidate');
 const { authorizedOrdinaryUser } = require('../utilities/utilities');
 const logger = require('../utilities/logger');
 
 resultRouter.get('/all', authorizedOrdinaryUser, async (req, res) => {
     try {
         logger.info("Retrieving the votes voted by the user");
-        const result = await User.aggregate([
-            {
-                $group: {
-                    _id: {
-                        $cond: {
-                            if: {
-                                $and: [
-                                    { $gte: ['$candidateVotedId', 0] }, // Check if candidateVotedId is greater than or equal to 0
-                                    { $ne: ['$candidateVotedId', null] }, // Check if candidateVotedId is not null
-                                ],
-                            },
-                            then: '$candidateVotedId', // Use candidateVotedId as _id
-                            else: 'noVotes', // Use 'noVotes' as _id if conditions are not met
-                        },
-                    },
-                    count: { $sum: 1 }, //Increment 1 for every Id
-                },
-            },
-            {
-                $lookup: {
-                    from: 'candidates',
-                    localField: '_id',
-                    foreignField: 'id',
-                    as: 'candidateInfo',
-                },
-            },
-            {
-                $project: {
-                    id: { $arrayElemAt: ['$candidateInfo.id', 0] }, //Extracting the first element of the id field
-                    name: {$arrayElemAt: ['$candidateInfo.name', 0]},//Extracting the first element of the name field
-                    count: { $ifNull: ['$count', 0] },  //Excluded when candidateVoted = null
-                },
-            },
-            {
-                $match: { id: { $exists: true } }, // Exclude candidates that don't exist in the candidate model
-            },
-            {
-                $sort: { id: 1 }, // Sort by id in ascending order
-            },
-        ]);
 
-        if (result.length > 0) {
-            const votes = result.reduce((total, entry) => total + entry.count, 0);
-            const results = result.map(entry => ({
-                id: entry.id,
-                name: entry.name,
-                votesReceived: entry.count,
-            }));
+        // Get all users who have voted
+        const votedUsers = await User.find({ candidateVotedId: { $gte: -1, $ne: null } });
 
-            const response = {
-                votes,
-                results,
-            };
-            logger.info("Voting result successfully obtained");
-            return res.status(200).json(response);
-        }
-        logger.info("No user has voted yet");
-        res.status(204).json({ votes: 0, results: [] });
+        // Create a map to count votes for each candidate
+        const candidateVoteCount = new Map();
+
+        votedUsers.forEach(user => {
+            const candidateId = user.candidateVotedId;
+            if (candidateVoteCount.has(candidateId)) {
+                candidateVoteCount.set(candidateId, candidateVoteCount.get(candidateId) + 1);
+            } else {
+                candidateVoteCount.set(candidateId, 1);
+            }
+        });
+
+        // Get candidate information for the candidates with votes
+        const allCandidates = await Candidate.find();
+
+        // Process the results
+        const votingResults = allCandidates.map(candidate => ({
+            id: candidate.id,
+            name: candidate.name,
+            votesReceived: candidateVoteCount.get(candidate.id) || 0,
+        }));
+
+        votingResults.sort((firstCandidate, secondCandidate) => firstCandidate.id - secondCandidate.id);
+        let allVotes = 0;
+        Array.from(candidateVoteCount.values()).forEach(n => {allVotes += n});
+
+
+        const response = {
+            votes: allVotes,
+            results: votingResults,
+        };
+
+        logger.info("Voting result successfully obtained");
+        res.status(200).json(response);
     } catch (error) {
         logger.error("An error occurred while getting the votes, details:", error);
         res.status(500).json({ error: error });
@@ -76,42 +57,26 @@ resultRouter.get('/candidateId=:id', authorizedOrdinaryUser, async (req, res) =>
     logger.info('Requested result for candidate with ID:', candidateId);
 
     if (!candidateId || isNaN(candidateId) || candidateId < 0) {
-        logger.error(`Candidate id ${candidateId} is invalid!`);
+        logger.error(`Invalid candidate ID: ${candidateId}`);
         return res.status(400).send({ error: "Invalid candidate ID. Please provide a valid candidate ID." });
     }
 
     try {
-        const result = await User.aggregate([
-            {
-                $match: {
-                    candidateVotedId: parseInt(candidateId),
-                },
-            },
-            {
-                $lookup: {
-                    from: 'candidates',
-                    localField: 'candidateVotedId',
-                    foreignField: 'id',
-                    as: 'candidateInfo',
-                },
-            },
-        ]);
+        // Get all users who voted for the specified candidate
+        const votedUsers = await User.find({ candidateVotedId: parseInt(candidateId) });
+        const candidateInfo = await Candidate.findOne({ id: candidateId });
 
-        if (result.length > 0) {
-            const votedUser = result[0];
-            if (votedUser.candidateInfo.length > 0) {
-                const candidateInfo = votedUser.candidateInfo[0];
-                const response = {
-                    id: candidateInfo.id,
-                    name: candidateInfo.name,
-                    votesReceived: result.length,
-                };
-                logger.info(`Result found for candidate. ID: ${candidateInfo.id}, name: ${candidateInfo.name}, votesReceived: ${result.length}`);
-                return res.status(200).json(response);
-            }
+        if (candidateInfo) {
+            const response = {
+                id: candidateInfo.id,
+                name: candidateInfo.name,
+                votesReceived: votedUsers.length,
+            };
+            logger.info(`Result found for candidate. ID: ${candidateInfo.id}, name: ${candidateInfo.name}, votesReceived: ${votedUsers.length}`);
+            return res.status(200).json(response);
         }
-        logger.info("No results found for the specified candidate");
-        res.status(204).json({ message: 'No results found for the specified candidate' });
+        logger.info(`Candidate with ID ${candidateId} does not exist!`);
+        res.status(400).json({ error: `Candidate with ID ${candidateId} does not exist!` });
     } catch (error) {
         logger.error("An error occurred while getting the votes for the specific candidate, details:", error);
         res.status(500).json({ error: error });
