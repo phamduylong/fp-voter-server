7f5e41324c11c5d55d0a9469778637cd66db2cd3
Long Pham <69810988+phamduylong@users.noreply.github.com>
2023-11-10 20:51:27 +0200

    FPV-50 add an endpoint to get user's candidate (#12)
    * FPV-50 add an endpoint to get user's candidate
    
    * FPV-50 fix HTTP code mistake

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = express.Router();
 const User = require('../models/User');
+const Candidate = require('../models/Candidate');
 const { authorizedOrdinaryUser } = require('../utilities/utilities');
 const logger = require('../utilities/logger');
 
@@ -26,7 +27,43 @@ userRouter.get('/id=:id', authorizedOrdinaryUser, async (req, res) => {
             return res.status(200).send( result );
         }
         logger.error("Couldn't find user with id: ", userId);
-        return res.status(400).send({ error: "User Not Found!" });
+        return res.status(404).send({ error: "User Not Found!" });
+    } catch (error) {
+        logger.error("Failed to get user information. Error: ", error);
+        return res.status(500).send({ error: error });
+    }
+});
+
+userRouter.get("/candidateVoted/id=:id", authorizedOrdinaryUser, async (req, res) => {
+    const userId = req.params.id;
+    if(userId === undefined || userId === null || userId < 0) {
+        logger.error(`User id ${userId} is invalid!`);
+        return res.status(400).send({ error: "User id is invalid!" });
+    }
+    try {
+        logger.debug("Getting user by id: ", userId);
+        const user = await User.find({ id: Number(userId) });
+        if (user.length === 1) {
+            const candidateVotedId = user[0].candidateVotedId;
+            if(candidateVotedId === undefined || candidateVotedId === null || candidateVotedId < 0) {
+                logger.error(`Candidate voted id ${candidateVotedId} is invalid!`);
+                return res.status(204).send({ });
+            }
+            const candidate = await Candidate.find({ id: Number(candidateVotedId) });
+            if (candidate.length === 1) {
+                const result = {
+                    id: candidate[0].id,
+                    name: candidate[0].name,
+                    message: candidate[0].message,
+                    img: candidate[0].img
+                };
+                logger.info("Candidate found successfully! Details:", result);
+                return res.status(200).send(result);
+            }
+            return res.status(204).send({ });
+        }
+        logger.error("Couldn't find user with id: ", userId);
+        return res.status(404).send({ error: "User Not Found!" });
     } catch (error) {
         logger.error("Failed to get user information. Error: ", error);
         return res.status(500).send({ error: error });
